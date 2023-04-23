b69605aaf40f04cea00473b1b549da26dabea38e
A SURJITHRAJA <106502056+Surjith-07@users.noreply.github.com>
2023-04-23 11:23:40 +0530

    Add files via upload

---
+++ src/rentcar/RentCar.java
@@ -0,0 +1,12 @@
+package rentcar;
+
+public class RentCar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
